1d94dfdae885bc331e32ae8a550db3859bf0885e
DennsHartstock <dennis.hartstock@web.de>
2022-04-13 05:36:39 +0200

    add Room CRUD annotations

---
+++ app/src/main/java/com/example/mycontacts/MyContactsDatabase.java
@@ -0,0 +1,8 @@
+package com.example.mycontacts;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Contact.class}, version = 1)
+public abstract class MyContactsDatabase extends RoomDatabase {
+}
